dd4647652927b89bb67d1e28ad2ea95400e64a7c
Washing-Mr8 <washing.22ml@gmail.com>
2024-11-18 15:09:28 +0100

    Commit inicial

---
+++ eventos_uni/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "AW_24"
+});
+
+module.exports = pool;
