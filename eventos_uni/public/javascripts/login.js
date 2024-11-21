711c60a7b04fe51e5a5506cc271f6475e1bc1da6
Washing-Mr8 <washing.22ml@gmail.com>
2024-11-21 14:51:01 +0100

    front del login

---
+++ eventos_uni/public/javascripts/login.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+ 
+ 
+    document.getElementById('mostrarLoginPassword').addEventListener('click', function() {
+        const passwordInput = document.getElementById('loginPassword');
+        const toggleBtn = document.getElementById('mostrarLoginPassword');
+        
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            toggleBtn.textContent = 'Ocultar';
+        } else {
+            passwordInput.type = 'password';
+            toggleBtn.textContent = 'Mostrar';
+        }
+    });
+
+
+});
