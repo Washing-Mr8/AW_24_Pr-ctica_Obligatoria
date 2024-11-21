374676e0862b04fe67935b82d211d9e3f61da88d
pzapico23 <125588835+pzapico23@users.noreply.github.com>
2024-11-21 22:32:41 +0100

    mas cosas

--- eventos_uni/routes/events.js
+++ eventos_uni/routes/events.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
    
 });
 router.post('/create',function(req,res){
-    const {eventTitle,eventType,eventDate,eventTime,eventLocation,eventCapacity,eventDescription,} = req.body;
+    const {eventTitle,eventType,eventDate,eventTime,eventLocation,eventCapacity,eventDescription,eventExact,eventDuration} = req.body;
     idORganizer ='1'; // SACARLO DE LAS SESIONES CUANDO ESTE
     pool.query('SELECT Rol FROM usuarios WHERE ID = ?', [idORganizer], (err,user) =>{
         if(err) throw err;
@@ -22,17 +22,25 @@ router.post('/create',function(req,res){
             pool.query('SELECT id FROM eventos WHERE Titulo = ?',[eventTitle], (err,check)=>{
                 if(err) throw err;
                 if(check.length == 0){
-                    pool.query('SELECT Hora FROM eventos WHERE Fecha = ? AND ', [eventDate], (err,time) =>{
-                        if(times.length != 0){
-                           times.forEach(time => {
-
-                           });
-                        }
-                        pool.query('INSERT INTO eventos (Titulo,Descripcion,Fecha,Hora,Ubicacion,Capacidad_Maxima,tipo,Organizador_ID) VALUES(?,?,?,?,?,?,?,?)',[eventTitle,eventDescription,eventDate,eventTime,eventLocation,eventCapacity,eventType,idORganizer], (err) =>{
-                            if(err) {
-                                throw err;
+                    pool.query('SELECT ID FROM facultades WHERE Nombre = ?', [eventLocation], (err,locationID) =>{
+                        pool.query('SELECT Hora, Duracion FROM eventos WHERE Fecha = ? AND IDfacultad = ? AND Ubicacion = ?', [eventDate, locationID.ID, eventExact], (err,repeated) =>{
+                            canInsert = true;
+                            if(repeated.length != 0){
+                                repeated.forEach(repeatedEvent => {
+                                    if(repeatedEvent.Hora + repeatedEvent.Duracion > eventTime || eventTime + eventDuration > repeatedEvent.Hora){
+                                        canInsert = false;
+                                    }
+                                });
                             }
-                            res.redirect('/viewEvents');
+                            if(canInsert){
+                                pool.query('INSERT INTO eventos (Titulo,Descripcion,Fecha,Hora,Ubicacion,Capacidad_Maxima,tipo,Duracion,Capacidad_Actual,IDfacultad,Organizador_ID) VALUES(?,?,?,?,?,?,?,?,?,?,?)'
+                                    ,[eventTitle,eventDescription,eventDate,eventTime,eventLocation,eventCapacity,eventType,eventDuration,0,locationID.ID,idORganizer], (err) =>{
+                                    if(err) {
+                                        throw err;
+                                    }
+                                    res.redirect('/viewEvents');
+                                });
+                            }
                         });
                     });
                 }
