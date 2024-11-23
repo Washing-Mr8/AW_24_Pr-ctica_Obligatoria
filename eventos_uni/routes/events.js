56891d08e48ebfde4fe7edf2c0e1b856926c47ba
pzapico23 <125588835+pzapico23@users.noreply.github.com>
2024-11-23 21:45:01 +0100

    update funciona

--- eventos_uni/routes/events.js
+++ eventos_uni/routes/events.js
@@ -24,14 +24,7 @@ router.post('/create',function(req,res){
                 if(check.length == 0){
                     pool.query('SELECT ID FROM facultades WHERE Nombre = ?', [eventLocation], (err,locationID) =>{
                         pool.query('SELECT Hora, Duracion FROM eventos WHERE Fecha = ? AND IDfacultad = ? AND Ubicacion = ?', [eventDate, locationID[0].ID, eventExact], (err,repeated) =>{
-                            canInsert = true;
-                            if(repeated.length != 0){
-                                repeated.forEach(repeatedEvent => {
-                                    if(repeatedEvent.Hora + repeatedEvent.Duracion > eventTime || eventTime + eventDuration > repeatedEvent.Hora){
-                                        canInsert = false;
-                                    }
-                                });
-                            }
+                            canInsert = checkTime(repeated,eventTime,eventDuration);
                             if(canInsert){
                                 pool.query('INSERT INTO eventos (Titulo,Descripcion,Fecha,Hora,Ubicacion,Capacidad_Maxima,tipo,Duracion,Capacidad_Actual,IDfacultad,Organizador_ID,facultad) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
                                     ,[eventTitle,eventDescription,eventDate,eventTime,eventExact,eventCapacity,eventType,eventDuration,0,locationID[0].ID,idORganizer,eventLocation], (err) =>{
@@ -58,19 +51,69 @@ router.post('/delete/:id', function(req,res){
 });
 router.post('/edit/:id', function(req,res){
     const eventId = req.params.id;
-  const {
-    eventTitle,
-    eventType,
-    eventDate,
-    eventTime,
-    eventDuration,
-    eventLocation,
-    eventExact,
-    eventCapacity,
-    eventDescription,
-  } = req.body;
+    const {
+        eventTitle,
+        eventType,
+        eventDate,
+        eventTime,
+        eventDuration,
+        eventLocation,
+        eventExact,
+        eventCapacity,
+        eventDescription,
+    
+    } = req.body
+    pool.query('SELECT * FROM eventos WHERE id = ?', [eventId], (err,bdEvent) =>{
+        if(err) throw err;
+        pool.query('SELECT ID FROM facultades WHERE Nombre = ?', [eventLocation], (err,locationID) =>{
+            if(err) throw err;
+            if(bdEvent[0].Titulo !== eventTitle)
+                bdEvent[0].Titulo = eventTitle;
+            if(bdEvent[0].tipo !== eventType)
+                bdEvent[0].tipo = eventType;
+            if(bdEvent[0].Fecha !== eventDate)
+                bdEvent[0].Fecha = eventDate;
+            if(bdEvent[0].Hora !== eventTime)
+                bdEvent[0].Hora = eventTime;
+            if(bdEvent[0].Duracion !== eventDuration)
+                bdEvent[0].Duracion = eventDuration;
+            if(bdEvent[0].facultad !== eventLocation)
+                bdEvent[0].facultad = eventLocation;
+            if(bdEvent[0].Ubicacion !== eventExact)
+                bdEvent[0].Ubicacion = eventExact;
+            if(bdEvent[0].Capacidad_Maxima !== eventCapacity)
+                bdEvent[0].Capacidad_Maxima = eventCapacity;
+            if(bdEvent[0].Descripcion !== eventDescription)
+                bdEvent[0].Descripcion = eventDescription;
+            if(bdEvent[0].IDfacultad !== locationID[0].ID)
+                bdEvent[0].IDfacultad = locationID[0].ID;
 
-  pool.query
+            pool.query('SELECT Hora, Duracion FROM eventos WHERE Fecha = ? AND IDfacultad = ? AND Ubicacion = ?', [bdEvent[0].Fecha, bdEvent[0].IFfacultad, bdEvent[0].Ubicacion], (err,repeated) =>{
+                canUpdate = checkTime(repeated,bdEvent[0].Hora,bdEvent[0].Duracion);
+                if(canUpdate){
+                   pool.query('UPDATE Eventos SET Titulo = ?, tipo = ?, Fecha = ?, Hora = ?, Duracion = ?, facultad = ?, Ubicacion = ?, Capacidad_Maxima = ?, Descripcion = ?, IDfacultad = ? WHERE ID = ? ',
+                    [bdEvent[0].Titulo,bdEvent[0].tipo,bdEvent[0].Fecha,bdEvent[0].Hora,bdEvent[0].Duracion,bdEvent[0].facultad,bdEvent[0].Ubicacion,bdEvent[0].Capacidad_Maxima,bdEvent[0].Descripcion,bdEvent[0].IDfacultad,eventId],
+                    (err) =>{
+                        res.redirect("/viewEvents");
+                    }
+                   );
+                }
+            });
+
+        });
+    });
 
 });
+
+function checkTime(events,eventTime,eventDuration){
+    canInsert = true;
+    if(events.length != 0){
+        events.forEach(repeatedEvent => {
+            if(repeatedEvent.Hora + repeatedEvent.Duracion > eventTime || eventTime + eventDuration > repeatedEvent.Hora){
+                canInsert = false;
+            }
+        });
+    }
+    return canInsert;
+}
 module.exports = router;
